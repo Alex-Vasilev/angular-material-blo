94aaa964d9911eab296dec58717543d29355bccf
Alex-Vasilev <black_funeral@list.ru>
2017-01-15 23:17:58 +0300

    ng-material-blog

---
+++ public_html/assets/js/newjavascript.js
@@ -0,0 +1,14 @@
+//var a= window.pageYOffset;
+
+window.onscroll = function(){
+var scrolled = window.pageYOffset|| document.documentElement.scrollTop;
+console.log(scrolled);
+
+//if(window.pageYOffset>40){
+//    document.getElementById('header').style.opacity = 0.3;
+//}
+}
+
+function Header(opacity){
+   this.opacity = opacity || 0.3; 
+}
